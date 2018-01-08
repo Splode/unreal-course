7b4d7ea3e07448c1f42a938bb8f9c60952570231
Christopher Murphy <christopherianmurphy@gmail.com>
2018-01-07 20:07:54 -0700

    Add project files.

---
+++ BullCowGame/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
+
+int main () {
+	PrintIntro();
+	PlayGame();
+	AskToPlayAgain();
+
+	cout << endl;
+	return 0;
+}
+
+void PrintIntro () {
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	return;
+}
+
+void PlayGame () {
+	constexpr int TRIES = 5;
+
+	for (int i = 0; i < TRIES; i++) {
+		string Guess = GetGuess();
+		cout << "Your guess: " << Guess << endl;
+	}
+}
+
+string GetGuess () {
+	string Guess = "";
+
+	cout << "\nEnter your guess: ";
+	getline(cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain () {
+	string Response = "";
+
+	cout << "Would you like to play again? ";
+	getline(cin, Response);
+	return true;
+}
